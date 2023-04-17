dca91052d55371fa6353bcdfa683519c18b509e4
Leonardo Lazarini <llazarini2@gmail.com>
2023-04-17 12:08:12 +0100

    Simple e2e test.

--- test/images/images.controller.e2e-spec.ts
+++ test/images/images.controller.e2e-spec.ts
@@ -1,37 +1,28 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { ImagesController } from '../../src/images/images.controller';
-import { ImagesService } from '../../src/images/images.service';
 import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
-import { ImagesModule } from '../../src/images/images.module';
+import { AppModule } from '../../src/app.module';
 
 describe('ImagesController', () => {
     let app: INestApplication;
     const service: any = { findRandom: () => ['test'] };
 
     beforeAll(async () => {
-        const moduleRef = await Test.createTestingModule({
-            imports: [ImagesModule],
-        })
-        .overrideProvider(ImagesService)
-        .useValue(service)
-        .compile();
-
-        app = moduleRef.createNestApplication();
-        await app.init();
+        const moduleFixture: TestingModule = await Test.createTestingModule({
+            imports: [AppModule],
+        }).compile()
+      
+        app = moduleFixture.createNestApplication()
+        await app.init()
     });
 
     it(`/GET random`, () => {
         return request(app.getHttpServer())
         .get('/images/random')
         .expect(200)
-        .expect({
-            data: service.findRandom(),
-        });
     });
 
     afterAll(async () => {
         await app.close();
     });
-
 });
